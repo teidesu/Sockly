d46dc43739aac3f8f9ac7d19530c3e047d451612
Max Sirenev <me@tei.su>
2019-11-13 19:15:57 +0300

    meh x3

--- rawige/__main__.py
+++
@@ -1,8 +0,0 @@
-from rawige.app import Rawige
-from PyQt5.QtWidgets import QApplication
-import sys
-
-app = QApplication(sys.argv)
-win = Rawige()
-win.show()
-sys.exit(app.exec())
--- rawige/utils/db_versions.py
+++
@@ -1,8 +0,0 @@
-DB_VERSIONS = {
-    1: [
-        'create table config (k text, v text)',
-        'create unique index config_uindex on config (k)',
-        'create table config_blobs (k text, v blob)',
-        'create unique index config_blobs_uindex on config_blobs (k)'
-    ]
-}
